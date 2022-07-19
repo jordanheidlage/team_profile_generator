1b32c09b92afff6d5a1a634bb0b19fc75e82dd10
Jordan Heidlage <jordanheidlage@jordans-air.lan>
2022-07-19 16:29:43 -0400

    created general layout

--- develop/__tests__/Employee.test.js
+++ develop/__tests__/Employee.test.js
@@ -1,11 +1,24 @@
 const Employee = require("../lib/Employee")
-describe("Employee", ()=>{
-    describe("test Employee's properties", ()=>{})
-        it ("test all properties", (=>{}))
-        const employee=new Employee('Jordan',1,
-        'jheidlage6@gmail.com')
-        expect(employee.name).toEqual('Jordan')
-        expect(employee.name).toEqual(1)
-        expect(employee.name).toEqual('jheidlage6@gmail.com')
-    describe("test Employee's methods", ()=>{})
+describe("Employee", () => {
+    describe("test Employee's properties", () => {
+        it("test all properties", () => {
+            const employee = new Employee('Phil', 1, 'ploy@yahoo.com')
+
+            expect(employee.name).toEqual('Phil')
+            expect(employee.id).toEqual(1)
+            expect(employee.email).toEqual('ploy@yahoo.com')
+
+        })
+
+    })
+
+    describe("test Employee's methods", () => {
+        it("test all methods", () => {
+            const employee = new Employee('brian', 2, 'brian@yahoo.com')
+            expect(employee.getName()).toEqual('brian')
+            expect(employee.getId()).toEqual(2)
+            expect(employee.getEmail()).toEqual('brian@yahoo.com')
+            expect(employee.getRole()).toEqual('Employee')
+        })
+    })
 })
